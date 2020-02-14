c6594592b6e384abe059745c0e2c75769cd51b2a
Bart Brendeke <bartbren1998@gmail.com>
2020-02-14 14:13:40 +0100

    Catkin workspace

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/bart/Documents/Wor/WoR-World-BP-Interface/src
+# Build directory: /home/bart/Documents/Wor/WoR-World-BP-Interface/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
