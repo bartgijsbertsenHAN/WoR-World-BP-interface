94658156f75beeea39a0e048c24aba27b26817e7
Bart Gijsbertsen <GLJ.Gijsbertsen@student.han.nl>
2020-03-09 14:48:27 +0100

    Removed angle calculation code from main

--- src/driver/src/main.cpp
+++ src/driver/src/main.cpp
@@ -14,17 +14,6 @@ int main(int argc, char** argv)
     std::cout << "Using " << argv[1] << " as port" << std::endl;
     HighLevelDriver highlevel = HighLevelDriver(argv[1]);
 
-    std::cout << "BASE: low = " << highlevel.degToPwm(-90, highlevel.getOffsetForJoint(BASE))
-        << ", high = " << highlevel.degToPwm(90, highlevel.getOffsetForJoint(BASE)) << std::endl;
-    std::cout << "SHOULDER: low = " << highlevel.degToPwm(-30, highlevel.getOffsetForJoint(SHOULDER))
-        << ", high = " << highlevel.degToPwm(90, highlevel.getOffsetForJoint(SHOULDER)) << std::endl;
-    std::cout << "ELBOW: low = " << highlevel.degToPwm(0, highlevel.getOffsetForJoint(ELBOW))
-        << ", high = " << highlevel.degToPwm(135, highlevel.getOffsetForJoint(ELBOW)) << std::endl;
-    std::cout << "WRIST_UP_DOWN: low = " << highlevel.degToPwm(-90, highlevel.getOffsetForJoint(WRIST_UP_DOWN))
-        << ", high = " << highlevel.degToPwm(90, highlevel.getOffsetForJoint(WRIST_UP_DOWN)) << std::endl;
-    std::cout << "WRIST_ROTATE: low = " << highlevel.degToPwm(-90, highlevel.getOffsetForJoint(WRIST_ROTATE))
-        << ", high = " << highlevel.degToPwm(90, highlevel.getOffsetForJoint(WRIST_ROTATE)) << std::endl;
-
     // highlevel.setJointAngle(BASE, 1000, 50.0);
     // highlevel.setJointAngle(GRIPPER, 2000, 50.0);
     // // highlevel.setTimeToComplete(2000);
